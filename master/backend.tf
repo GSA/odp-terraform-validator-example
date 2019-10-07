2b5163921ab4a1490a1bd49adfba9ecc2ec5e8d8
RobertJLupinek <9854828608@GSA.GOV>
2019-10-07 17:29:31 -0400

    Fixing CircleCI errors.

--- master/backend.tf
+++ master/backend.tf
@@ -3,6 +3,6 @@ terraform {
     credentials = "~/gcloud-service-key.json"
     # Cannot interpolate, so we will feed this in on the commandline for interpolation 
     #bucket      = "your-state-bucketname"
-    prefix  = "terraform/state/odp-tf-validator-example"
+    #prefix  = "terraform/state/odp-tf-validator-example"
   }
 }
